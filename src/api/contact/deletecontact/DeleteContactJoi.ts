5500890ed0e8065c51bbb815276879575d11f1c8
pijush@antino.com <pijush@antino.com>
2025-01-26 19:31:16 +0530

    Remove contact-related API endpoints and validation schemas

--- src/api/contact/deletecontact/DeleteContactJoi.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-import ErrorUtility from "../../../domain/constants/message/ErrorUtility";
-
-const DeleteContactJoi = Joi.object({
-  id: Joi.string().required().messages(ErrorUtility.joiHelper("id", "string", false)),
-});
-
-export default DeleteContactJoi;
