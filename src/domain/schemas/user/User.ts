f18c1fdb04aa222cc6b0a8a2b8f455801303ff89
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-02-20 15:06:24 +0530

    Implement user management API with Create, Read, Update, and Delete functionalities

---
+++ src/domain/schemas/user/User.ts
@@ -0,0 +1,65 @@
+import { Model, DataTypes } from "sequelize";
+import { postgresConnector } from "../../../core/utils/absoluteFilePath";
+
+const validRoles = ["admin", "manager", "user"] as const;
+type RoleType = (typeof validRoles)[number];
+
+class UserModel extends Model {
+  id!: number;
+  name!: string;
+  email!: string;
+  role!: RoleType;
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+UserModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 50], // Ensuring a reasonable name length
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    role: {
+      type: DataTypes.ENUM(...validRoles),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize: postgresConnector,
+    modelName: "user",
+    tableName: "user",
+    timestamps: true,
+    paranoid: true, // Enables soft delete
+  },
+);
+
+export default UserModel;
