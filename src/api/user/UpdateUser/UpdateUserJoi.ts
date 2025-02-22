4f3b4e1182796eed81d77f3044bbefe7bd4a3ae3
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-02-22 22:53:24 +0530

    Update role validation in UpdateUser schema to allow 'super_admin' option

--- src/api/user/UpdateUser/UpdateUserJoi.ts
+++ src/api/user/UpdateUser/UpdateUserJoi.ts
@@ -5,10 +5,13 @@ const UpdateUserJoi = Joi.object({
   id: Joi.string().required().messages(ErrorUtility.joiHelper("id", "number", false)),
   name: Joi.string().optional().messages(ErrorUtility.joiHelper("name", "string", false)),
   email: Joi.string().optional().email().messages(ErrorUtility.joiHelper("email", "email", false)),
-  role: Joi.string().optional().valid("admin", "member").messages(ErrorUtility.joiHelper("role", "string", false)),
+  role: Joi.string()
+    .optional()
+    .allow("admin", "super_admin", "member")
+    .messages(ErrorUtility.joiHelper("role", "string", false)),
   status: Joi.string()
     .optional()
-    .valid("active", "inactive")
+    .allow("active", "inactive")
     .messages(ErrorUtility.joiHelper("status", "string", false)),
 });
 
