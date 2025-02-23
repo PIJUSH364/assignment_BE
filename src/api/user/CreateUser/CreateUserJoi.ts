df23cac0145b735935eb83b1e2c0e1575bc8e016
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-02-23 18:27:21 +0530

    Allow 'super_admin' role in CreateUser schema validation

--- src/api/user/CreateUser/CreateUserJoi.ts
+++ src/api/user/CreateUser/CreateUserJoi.ts
@@ -4,7 +4,7 @@ import ErrorUtility from "../../../domain/constants/message/ErrorUtility";
 const CreateUserJoi = Joi.object({
   name: Joi.string().required().messages(ErrorUtility.joiHelper("name", "string", false)),
   email: Joi.string().email().messages(ErrorUtility.joiHelper("email", "email", false)),
-  role: Joi.string().valid("admin", "member").messages(ErrorUtility.joiHelper("role", "string", false)),
+  role: Joi.string().allow("admin", "member", "super_admin").messages(ErrorUtility.joiHelper("role", "string", false)),
 });
 
 export default CreateUserJoi;
