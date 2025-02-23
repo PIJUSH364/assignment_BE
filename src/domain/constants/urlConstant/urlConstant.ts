0c797c3e89a546efa905eddf54440b83224bb370
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-02-23 21:07:57 +0530

    Remove TicketBooking schemas and related repository files

--- src/domain/constants/urlConstant/urlConstant.ts
+++ src/domain/constants/urlConstant/urlConstant.ts
@@ -1,17 +1,4 @@
 const urlConstant = {
-  tickets: {
-    book: "/book",
-    cancel: "/cancel/:ticketId",
-    booked: "/booked/:ticketId",
-    available: "/available/:trainId",
-    final_chart_prepared: "/final_chart_prepared/:trainId",
-  },
-  passenger: {
-    create_passenger: "/create_passenger",
-  },
-  train: {
-    create_trainDetails: "/create_trainDetails",
-  },
   user: {
     create_user: "/create_user",
     update_user: "/update_user",
